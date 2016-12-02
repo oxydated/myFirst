4967de21092cfd866beaf6ffbe547200c90808be
oxydated <oxydation@gmail.com>
2016-12-02 00:14:12 -0200

    Some code for interactivity - camera rotation.

---
+++ interactivity.cpp
@@ -0,0 +1,34 @@
+#include "interactivity.h"
+
+static bool lButtonPressed = false;
+static int winX = 0;
+static int winY = 0;
+
+void setButtonPressed( bool isPressed) {
+	lButtonPressed = isPressed;
+}
+
+bool getButtonPressed() {
+	return lButtonPressed;
+}
+
+void setWinX(int x) {
+	if (lButtonPressed) {
+		winX = x;
+	}
+}
+
+void setWinY(int y) {
+	if (lButtonPressed) {
+		winY = y;
+	}
+}
+
+int getWinX() {
+	return winX;
+}
+
+int getWinY() {
+	return winY;
+}
+
---
+++ interactivity.h
@@ -0,0 +1,19 @@
+#ifndef __INTERACTIVITY_HEADER__
+#define __INTERACTIVITY_HEADER__
+
+
+void setButtonPressed(bool isPressed);	
+
+bool getButtonPressed();
+
+void setWinX(int x);
+
+void setWinY(int y);
+
+int getWinX();
+
+int getWinY();
+
+#endif // !__INTERACTIVITY_HEADER__
+
+
